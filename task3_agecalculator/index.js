45eb2952c92a673a9412d118116ceb8d6429728a
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2023-10-14 12:01:04 +0530

    completed task 3

--- task3_agecalculator/index.js
+++ task3_agecalculator/index.js
@@ -1,4 +1,5 @@
 const uesrInput = document.querySelector('input')
+const result = document.querySelector('.result')
 
 uesrInput.max = new Date().toISOString().split("T")[0]
 
@@ -7,36 +8,41 @@ const getDaysOfMonth = (year, month) => {
 }
 
 const calculateAge = () => {
-    const birthDate = new Date(uesrInput.value)
+    if (uesrInput.value !== '') {
+        const birthDate = new Date(uesrInput.value)
+        
+        const d1 = birthDate.getDate()
+        const m1 = birthDate.getMonth() + 1
+        const y1 = birthDate.getFullYear()
+        
+        const today = new Date()
 
-    const d1 = birthDate.getDate()
-    const m1 = birthDate.getMonth() + 1
-    const y1 = birthDate.getFullYear()
+        let d2 = today.getDate()
+        let m2 = today.getMonth() + 1
+        let y2 = today.getFullYear()
+
+        let d3, m3, y3
+
+        if(d2>=d1){
+            d3 = d2 - d1
+        }else{
+            d3 = (d2 + getDaysOfMonth(y2, m2)) - d1
+            m2--
+        }
+
+        if(m2>=m1){
+            m3 = m2 - m1
+        }else{
+            m3 = (m2 + 12) - m1
+            y2--
+        }
+
+        y3 = y2 - y1
+
+        result.querySelector('span').innerHTML = `Age : ${y3} years, ${m3} months, ${d3} days`
+    }else{ 
+        alert("Please Select Your Date of Birth...!")
+    }
     
-    const today = new Date()
-
-    let d2 = today.getDate()
-    let m2 = today.getMonth() + 1
-    let y2 = today.getFullYear()
-
-    let d3, m3, y3
-
-    if(d2>=d1){
-        d3 = d2 - d1
-    }else{
-        d3 = (d2 + getDaysOfMonth(y2, m2)) - d1
-        m2--
-    }
-
-    if(m2>=m1){
-        m3 = m2 - m1
-    }else{
-        m3 = (m2 + 12) - m1
-        y2--
-    }
-
-    y3 = y2 - y1
-
-    console.log(`${y3} years, ${m3} months, ${d3} days`);
 }
 
